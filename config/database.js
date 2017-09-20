8fb0a273d359ecec467ac4535f60d29394b413e5
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-09-20 10:21:31 +0100

    update config

--- config/database.js
+++ config/database.js
@@ -50,11 +50,11 @@ module.exports = {
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
-      password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
-      database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname)
+      host: CLEARDB_DATABASE_URL.host,
+      // port: Env.get('DB_PORT', ''),
+      user: CLEARDB_DATABASE_URL.username,
+      password: CLEARDB_DATABASE_URL.password,
+      database: CLEARDB_DATABASE_URL.pathname
     }
   },
 

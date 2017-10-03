0721a110979e719fd16d75b45347d2746df6b262
Harminder Virk <virk.officials@gmail.com>
2017-10-03 22:26:51 +0530

    chore(package): add vow provider

--- start/app.js
+++ start/app.js
@@ -32,7 +32,8 @@ const providers = [
 |
 */
 const aceProviders = [
-  '@adonisjs/lucid/providers/MigrationsProvider'
+  '@adonisjs/lucid/providers/MigrationsProvider',
+  '@adonisjs/vow/providers/VowProvider'
 ]
 
 /*
